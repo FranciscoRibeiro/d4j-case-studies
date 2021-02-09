a48eabdc6bfe0acfcb1f3dd5e42cedc960cd15ff
FranciscoRibeiro <kikoribeiro95@gmail.com>
2021-02-09 13:12:25 +0000

    Add script to infer mutations in changed files and respective inferences

---
+++ infer_mut_ops.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script gets as input:
+# 1. the directory with the files for which we wish to infer mutations
+# 2. the path to Morpheus
+# 3. file to output the inference report
+
+DIR="$1"
+MORPHEUS="$2"
+OUT_FILE="$3"
+
+java_sources="$(find "$DIR/previous" -name "*\.java" | sed -e "s|^$DIR/previous/||")"
+
+for file in ${java_sources[@]}
+do
+	if [[ -f "$DIR/next/$file" ]]
+	then
+		result="$(timeout 1m java -jar "$MORPHEUS" "$DIR/previous/$file" "$DIR/next/$file")"
+		exit_code="$?"
+		case "$exit_code" in
+			0)
+				echo "$result" >> "$OUT_FILE"
+                ;;
+            1)
+                echo "Error,$DIR/previous/$file,$DIR/next/$file" >> "$OUT_FILE"
+                ;;
+            124)
+                echo "Timeout,$DIR/previous/$file,$DIR/next/$file" >> "$OUT_FILE"
+                ;;
+        esac
+	fi
+done
+
